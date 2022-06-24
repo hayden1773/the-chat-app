e2f3d66996ea87051ac9f5a45303b281c398b8a2
Hayden Kiltoff <hayden060@gmail.com>
2022-06-24 15:36:12 -0700

    componets added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
-import { ChatEngine } from "react-chat-engine";
-
 import './App.css'
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import  ChatFeed  from './components/chatfeed';
+
+const App = () => {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='hayden1773'
+			userSecret='password'
+			projectID='45c6878e-6189-4835-a9dc-60cd9969e6db'
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} />}
+		/>
+	);
+}
+
+export default App;
