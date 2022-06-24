4138170be80ce8c4b1c083dc9463e9d607e4fdc9
Hayden Kiltoff <hayden060@gmail.com>
2022-06-24 15:56:39 -0700

    generic message structure

--- src/components/chatfeed.jsx
+++ src/components/chatfeed.jsx
@@ -1,5 +1,47 @@
+import MessageForm from "./MessageForm";
+import TheirMessage from "./TheirMessage";
+import MyMessage from "./MyMessage";
+
 const ChatFeed = (props) => {
-    console.log(props);
+    // structure of props
+    const { chats, activeChat, userName, messages } = props;
+    // Looking for specific active chat
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        // fetch all messages
+        const keys = Object.keys(messages);
+        // console.log(keys);
+        // render messages
+        return keys.map((key, index) => {
+            // loop over messages with specific key
+            const message = messages[key];
+            // Last message sent - if there are messages, find the last one > if else
+            const lastMessageKey = index ===0 ? null : keys[index -1];
+            // Is this the same message?
+            const isMyMessage = userName === message.sender.username;
+
+            return(
+                <div key={'msg_${index}'} style={{width: '100%'}}>
+                    <div className="message-block">
+                        {
+                           isMyMessage
+                           ? <MyMessage />
+                           : <TheirMessage />
+                        }
+
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                        
+                    </div>
+                </div>
+            )
+
+        })
+    }
+
+    // console.log(chat, userName, messages)
+    // console.log(props);
 
     return(
         <div>
