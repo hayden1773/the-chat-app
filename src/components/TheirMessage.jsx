457020c044b4889b1e76a4e6fdc78219417a941f
Hayden Kiltoff <hayden060@gmail.com>
2022-06-26 15:58:46 -0700

    their message formatting

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,10 +1,32 @@
-const TheirMessage = ({lastMessage, message}) => {
+const TheirMessage = ({ lastMessage, message }) => {
     // Find out if it is the first message by the user - boolean value
 
     const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div
+                    className="message-avatar"
+                    style={{ backroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+            {
+                message?.attachments?.length > 0
+                    ? (
+                        <img
+                            src={message.attachments[0].file}
+                            alt="message-attachment"
+                            className="message-image"
+                            style={{ marginLeft: isFirstMessageByUser ? '4px': '48px' }}
+                        />
+                    ) : (
+                        <div className="message" style={{ flaot: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px': '48px'  }}>
+                            {message.text}
+                        </div>
+                    )
+
+            }
+            
         </div>
     )
 }
